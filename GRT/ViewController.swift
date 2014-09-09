25c2cfa63c4b312dfff7098e5af2e77646fa9657
Yundi Huang <kenkaka.hyd@gmail.com>
2014-09-09 21:01:23 +0800

    Initial Commit

---
+++ GRT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GRT
+//
+//  Created by Ken Huang on 2014-09-09.
+//  Copyright (c) 2014 Ken Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
