00914b33947796118543e4d50f253451d14686af
Yundi Huang <kenkaka.hyd@gmail.com>
2014-10-01 20:45:03 -0400

    refactor GRTDate to have better data structure.

--- GRT/GRTDate.h
+++ GRT/GRTDate.h
@@ -9,10 +9,11 @@
 #import <Foundation/Foundation.h>
 
 @interface GRTDate : NSObject
-@property (nonatomic,readonly) NSInteger hour;
-@property (nonatomic,readonly) NSInteger minute;
-@property (nonatomic,readonly) NSTimeInterval second;
+@property (nonatomic,readonly) NSInteger hour;  /* 0 - 24 H */
+@property (nonatomic,readonly) NSInteger minute;    /*0 - 60 Min */
+@property (nonatomic,readonly) NSTimeInterval timeIntervalSinceMidnight; /* For easy computation, use sec from 00:00:00 til now as measure */
 
 - (id) initWithString:(NSString*) time;
+- (void)consumeTimeInterval:(NSTimeInterval)interval;
 
 @end
