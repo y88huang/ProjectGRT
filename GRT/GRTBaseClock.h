e5b951a752708bef3d6f3ea2c36c7e6bf5a54774
Yundi Huang <kenkaka.hyd@gmail.com>
2014-10-01 20:43:43 -0400

    add GRTBaseClock singleton for time management.

---
+++ GRT/GRTBaseClock.h
@@ -0,0 +1,19 @@
+//
+//  GRTBaseClock.h
+//  GRT
+//
+//  Created by Ken Huang on 2014-10-01.
+//  Copyright (c) 2014 Ken Huang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class GRTDate;
+
+@interface GRTBaseClock : NSObject
+
++ (instancetype)sharedInstance;
+
+@property (strong, nonatomic, readonly) GRTDate *date;
+
+@end
